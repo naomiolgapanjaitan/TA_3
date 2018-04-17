7883d16207f759a0966b4b3cb524d8d199efb433
Naomi <naomiolgapanjaitan@gmail.com>
2018-04-17 21:20:39 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -11,3 +11,28 @@ CREATE TABLE `status_surat` (
   `nama` varchar(15) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE pengajuan_surat(
+id int NOT NULL,
+no_surat varchar(25) NOT NULL,
+username_pengaju varchar(25) NOT NULL,
+tanggal_mohon date NOT NULL,
+id_jenis_surat int NOT NULL,
+keterangan text ,
+alasan_izin text,
+tanggal_mulai_izin date,
+tanggal_selesai_izin date ,
+id_matkul_terkait int  ,
+username_pegawai varchar(20),
+id_status_surat int not null,
+PRIMARY KEY (id),
+FOREIGN KEY (username_pengaju) REFERENCES user_account(username) ON UPDATE
+CASCADE ON DELETE CASCADE,
+ FOREIGN KEY (id_jenis_surat) REFERENCES jenis_surat(id) ON UPDATE
+CASCADE ON DELETE CASCADE,
+     FOREIGN KEY (id_matkul_terkait) REFERENCES mata_kuliah(id) ON UPDATE
+CASCADE ON DELETE CASCADE,
+      FOREIGN KEY (id_status_surat) REFERENCES status_surat(id) ON UPDATE
+CASCADE ON DELETE CASCADE
+);
